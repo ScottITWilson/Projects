e78ec39f9a6b4f7f7bb87b6ec65dc50db3e6d0c7
ScottITWilson <164669671+ScottITWilson@users.noreply.github.com>
2024-03-23 15:05:34 -0300

    Update Weather.py
    Edited Comments

--- Weather.py
+++ Weather.py
@@ -8,12 +8,10 @@ request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
 response = requests.get(request_url)
 
 if response.status_code == 200:
-    data = response.json()
-    #print (data)
+    data = response.json() #convert data to a readable format
     weather = data['weather'][0]['description']
     temperature = round(data['main']['temp'] - 273.15, 2) #-273.15 to convert from Kelvin to Celcius
     print("The weather for", city, "is:\n", weather, "\n", temperature, "C")
-    #print (weather)
-    #print (temperature)
+
 else:
     print("An error occured")
