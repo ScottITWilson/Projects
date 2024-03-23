45e4ab0b25a5ef6a77c11a26eaca0a49694a7076
ScottITWilson <164669671+ScottITWilson@users.noreply.github.com>
2024-03-23 15:03:45 -0300

    Added weather API project
    Submitted this Weather fetching script to build my projects folder

---
+++ Weather.py
@@ -0,0 +1,19 @@
+import requests
+
+API_KEY = '40f52b19086428c837e6c896c3b53ad7'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+city = input("Enter city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    #print (data)
+    weather = data['weather'][0]['description']
+    temperature = round(data['main']['temp'] - 273.15, 2) #-273.15 to convert from Kelvin to Celcius
+    print("The weather for", city, "is:\n", weather, "\n", temperature, "C")
+    #print (weather)
+    #print (temperature)
+else:
+    print("An error occured")
